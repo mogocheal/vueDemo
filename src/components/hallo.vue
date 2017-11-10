57a360e98278891ef11323a46fa1bc6ccb18655d
xjm <46959838@qq.com>
2017-11-10 17:56:35 +0800

    first

---
+++ src/components/hallo.vue
@@ -0,0 +1,12 @@
+<template>
+      <div>
+      this is my  vue test<span>{{message}}</span>
+      </div>
+</template>
+
+<script>
+      export default {
+        name: 'test',
+        data() {  return{ message:'test 1561616'}  }
+      }
+</script>
